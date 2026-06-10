a94003133d4780bbb0b7c5d4d73395e93cf0f9a4
DirmdsLab <141147147+DirmdsLab@users.noreply.github.com>
2026-06-11 06:53:11 +0700

    hmm

--- user/File/Script/hyprland/hyprlock/hyprlock-idle.sh
+++ user/File/Script/hyprland/hyprlock/hyprlock-idle.sh
@@ -3,4 +3,7 @@ echo "[INFO] $(date "+%F %T") Lock Idle..." >> /tmp/lockscreenstyle.log
 
 notify-send "lock idle"
 
+sleep 0.5x
+
 hyprlock --config ~/File/Script/hyprland/hyprlock/hyprlock-idle.conf
+
