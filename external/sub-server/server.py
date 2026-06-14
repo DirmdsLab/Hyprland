0ca817d38dc2f658c46c59210ebf15c08dd9f2ca
DirmdsLab <141147147+DirmdsLab@users.noreply.github.com>
2026-06-14 23:14:57 +0700

    port

--- external/sub-server/server.py
+++ external/sub-server/server.py
@@ -109,4 +109,4 @@ if __name__ == "__main__":
     print(" - /translate_sub")
     print("====================================\n")
 
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5010, debug=False)
