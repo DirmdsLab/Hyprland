1e670992c8b1c935e73403713c5188ee63963782
DirmdsLab <141147147+DirmdsLab@users.noreply.github.com>
2026-07-22 19:16:03 +0700

    log

--- user/File/Script/hyprland/hyprlock/hyprlock-idle.sh
+++ user/File/Script/hyprland/hyprlock/hyprlock-idle.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
-echo "[INFO] $(date "+%F %T") Lock Idle..." >> /tmp/lockscreenstyle.log 
+
+LOG="/tmp/lockscreenstyle.log"
+
+echo "[INFO] $(date "+%F %T") Lock Idle..." >> "$LOG"
 
 notify-send -t 1000 "lock idle"
 
 sleep 2
 
-hyprlock --config ~/File/Script/hyprland/hyprlock/hyprlock-idle.conf
+echo "[INFO] $(date "+%F %T") Starting hyprlock..." >> "$LOG"
+hyprlock --config ~/File/Script/hyprland/hyprlock/hyprlock-idle.conf >>"$LOG" 2>&1
+STATUS=$?
+echo "[INFO] $(date "+%F %T") hyprlock exited with status $STATUS" >> "$LOG"
 
 notify-send "unlock"
